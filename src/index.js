5ea5d333195d821fb2e189f0453486fe97ee3fc6
Carlos Mertens <mertenscarlos@yahoo.com>
2021-05-07 23:20:15 +0200

    Create simple App component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Add to workaround reloading issues
+if (module.hot) {
+  module.hot.accept();
+}
+
 // Create a react component
 const App = function () {
-  return <div>Hi there!</div>;
+  const buttonText = 'Click me!';
+  const labelText = 'Enter name:';
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the browser
